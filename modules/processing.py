0c06e328c067faa49d602202ef03910f94fc1ad0
16Shadows <egor.0.00@mail.ru>
2024-06-07 22:23:09 +0500

    feat: Добавлена функция для генерации сетки точек для сэмплирования.

--- modules/processing.py
+++ modules/processing.py
@@ -1,5 +1,7 @@
 import geopandas
 import pyproj
+import numpy as np
+import shapely
 from typing import Tuple, cast, Final
 
 MSK_48_CRS : Final[str] = '+proj=tmerc +lat_0=0 +lon_0=38.48333333333 +k=1 +x_0=1250000 +y_0=-5412900.566 +ellps=krass +towgs84=23.57,-140.95,-79.8,0,0.35,0.79,-0.22 +units=m +no_defs'
@@ -29,3 +31,13 @@ def project_geometry(df: geopandas.GeoDataFrame, crs: str) -> geopandas.GeoDataF
 
 def wgs84_point_to_crs(point: Tuple[float, float], crs: str) -> Tuple[float, float]:
     return cast(Tuple[float, float], pyproj.Transformer.from_crs('EPSG:4326', crs, always_xy=True,).transform(*point, errcheck=True))
+
+def generate_sampling_grid(leftTop: Tuple[int, int], stepSize: int, columnCount: int, rowCount: int) -> geopandas.GeoDataFrame:
+    if columnCount < 1:
+        raise ValueError('columnCount should be greater than 0')
+    elif rowCount < 1:
+        raise ValueError('rowCount should be greater than 0')
+    elif stepSize < 1:
+        raise ValueError('stepSize should be greater than 0')
+
+    return geopandas.GeoDataFrame([shapely.Point(x, y) for y in range(leftTop[1] - stepSize//2, leftTop[1] - stepSize//2 - stepSize*rowCount, -stepSize) for x in range(leftTop[0] + stepSize//2, leftTop[0] + stepSize//2 + stepSize*columnCount, stepSize)])
