8da6f57e97305703790c52dda17562200500e320
meckhonoshinkv <kvmekhonoshin@edu.hse.ru>
2024-06-11 19:22:09 +0500

    feat: Изменен формат выгрузки данных с GeoJSON на TXT
    В этом коммите был изменен формат выгрузки данных карты высот. Вместо сохранения данных в файл GeoJSON, как было раньше, теперь данные сохраняются в текстовый файл. Это изменение было внесено для соответствия требованиям задачи.
    
    Для реализации этого изменения была добавлена функция height_map_to_lists из модуля processing, которая преобразует GeoDataFrame с картой высот в список списков. Затем в файл heightmap.txt записываются метаданные (количество столбцов, строк, координаты левого нижнего угла, размер ячейки и единицы измерения), а также сами данные карты высот в виде списка списков.

--- modules/geojson_to_heightmap.py
+++ modules/geojson_to_heightmap.py
@@ -2,7 +2,7 @@
 # 1. Задайте нужную область, изменив координаты `left_bottom` и `right_top`.
 # 2. При необходимости, отрегулируйте шаг сэмплирования (`step_size`).
 # 3. Запустите скрипт.
-# 4. Результирующая карта высот будет сохранена в файл `heightmap.geojson` в том же каталоге.
+# 4. Результирующая карта высот будет сохранена в файл `heightmap.txt` в том же каталоге.
 
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -15,10 +15,8 @@ from processing import (
     generate_height_map,
     MSK_48_CRS,
     wgs84_point_to_crs,
+    height_map_to_lists,
 )
-
-def save_heightmap(heightmap, filename):
-    heightmap.to_file(filename, driver='GeoJSON')
 
 if __name__ == "__main__":
     # Задаем координаты левого нижнего и правого верхнего углов области интереса
@@ -36,25 +34,38 @@ if __name__ == "__main__":
     df_polygons = contours_to_polygons(df_projected)
 
     # Вычисляем размер сетки сэмплирования
-    step_size = 50  # Шаг сэмплирования (в метрах) - расстояние между точками сетки
-    column_count = int((right_top_projected[0] - left_bottom_projected[0]) // step_size) + 1 # Количество столбцов в сетке
-    row_count = int((right_top_projected[1] - left_bottom_projected[1]) // step_size) + 1 # Количество строк в сетке
+    step_size = 10  # Шаг сэмплирования (в метрах) - расстояние между точками сетки
+    column_count = 2000  # Количество столбцов в сетке
+    row_count = 2000  # Количество строк в сетке
 
     # Генерируем сетку сэмплирования
     sampling_grid = generate_sampling_grid(
         leftTop=(int(left_bottom_projected[0]), int(right_top_projected[1])),  # Координаты левого верхнего угла сетки
-        stepSize=step_size, # Шаг сэмплирования (расстояние между точками)
-        columnCount=column_count, #583 Количество столбцов в сетке
-        rowCount=row_count, #436 Количество строк в сетке
-        crs=MSK_48_CRS, # Система координат сетки
+        stepSize=step_size,  # Шаг сэмплирования (расстояние между точками)
+        columnCount=column_count,  # Количество столбцов в сетке
+        rowCount=row_count,  # Количество строк в сетке
+        crs=MSK_48_CRS,  # Система координат сетки
     )
 
     # Присваиваем высоты точкам сетки
     heightmap = generate_height_map(df_polygons, sampling_grid)
-    heightmapCrs = heightmap.to_crs(epsg=4326)
 
-    # Сохраняем карту высот в файл
-    save_heightmap(heightmapCrs, 'heightmap.geojson')
+    # Получаем карту высот в виде списка списков
+    height_lists = height_map_to_lists(heightmap)
+
+    # Открываем файл для записи
+    with open('heightmap.txt', 'w', encoding='utf-8') as file:
+        # Записываем метаданные
+        file.write(f"ncols {column_count}\n")
+        file.write(f"nrows {row_count}\n")
+        file.write(f"xllcorner {int(left_bottom_projected[0])}\n")
+        file.write(f"yllcorner {int(left_bottom_projected[1])}\n")
+        file.write(f"cellsize {step_size}\n")
+        file.write("NODATA_value -99999     Unit μg/m3\n")
+
+        # Записываем данные карты высот
+        for row in height_lists:
+            file.write(' '.join(map(str, row)) + '\n')
 
     # Визуализируем данные
     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
